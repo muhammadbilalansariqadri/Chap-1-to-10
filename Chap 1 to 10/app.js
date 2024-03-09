571bc735d0a5d836e4181860cae581f13ef08962
Bilal Ansari <161131544+muhammadbilalansariqadri@users.noreply.github.com>
2024-03-09 12:52:48 +0500

    Add files via upload

---
+++ Chap 1 to 10/app.js
@@ -0,0 +1,79 @@
+// ALerts/////////////////////////
+
+// alert("Error! Please enter a valid password.")
+
+// alert("Welcome to JS Land. \n Happy Coding")
+
+// alert("Hello I can run JS through my web browser console")
+// console.log(alert)
+
+// Variable for Strings/////////////////////////
+
+// var x = "user.name"
+// console.log(x)
+
+// var myname = "Bilal"
+// console.log(myname)
+
+// var message = "Hello World"
+// alert(message)
+
+// var username = "Bilal"
+// var age = "24 years old"
+// var course = "Certified Web dev"
+// alert(username)
+// alert(age)
+// alert(course)
+
+// var message = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(message) 
+
+
+// var message = "my email address is "
+// var emailaddress ="example@example.com"
+// alert(message+emailaddress)
+
+
+// var book ="“A smarter way to learn JavaScript”."
+// alert(book)
+
+// var x = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(x)
+
+// Variable for Numbers
+
+// var age = 10
+// var x = " years old"
+// alert(age+x)
+
+// var message = "You have visited "
+// x = 14
+// end = " times"
+// alert(message+x+end)
+
+// var firstName = "Bilal",age= 15, lastName="Ansari"
+// alert(firstName+lastName+age)
+
+// 5 legal & 5 illegal
+
+// my Variable 4myVariable myVariable!
+// my_variable my$variable
+
+// MATH EXPRESSIONS
+
+// var x = 10
+// var y = 10
+// console.log(x+y)
+
+// var x = 10
+// var y = 5
+// console.log(x-y)
+
+// var x = 10
+// var y = 5
+// console.log(x/y)
+
+// var x = 10
+// var y = 5
+// console.log(x*y)
+
